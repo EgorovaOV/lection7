66610095b357685dda3495e7db632d5ecc3f4458
Olga <ove2501@mail.ru>
2022-01-11 23:47:08 +0500

    add lection7

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// собрать строку с числами от a до b, a<b
+
+string NewNum(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NewRec (int a, int b)
+{
+    if (a<=b) return $"{a} "+ NewRec (a+1, b);
+    else return String.Empty;
+      
+}
+
+Console.WriteLine(NewNum(1, 10));
+Console.WriteLine(NewRec(1, 10));
+
