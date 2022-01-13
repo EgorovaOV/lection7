e5a9f3abceb5306599f36a88be6fdadd874f6097
Olga <ove2501@mail.ru>
2022-01-13 23:22:58 +0500

    add lection77

--- Program.cs
+++ Program.cs
@@ -20,3 +20,23 @@ string NewRec (int a, int b)
 Console.WriteLine(NewNum(1, 10));
 Console.WriteLine(NewRec(1, 10));
 
+// собрать строку с числами от a до b, a>b
+string NewNumm(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NewRecc (int a, int b)
+{
+    if (a>=b) return $"{a} " + NewRecc(a-1, b);
+    else return String.Empty;
+      
+}
+
+Console.WriteLine(NewNumm(10, 1));
+Console.WriteLine(NewRecc(10, 1));
